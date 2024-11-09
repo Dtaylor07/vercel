e739a757f86422d7d52f04b6ad16ab6002e9d898
Dtaylor07 <dhavaltailor_07@hotmail.com>
2024-11-09 15:44:47 -0800

    initial run

---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    vercel = {
+      source = "vercel/vercel"
+      version = "2.3.0"
+    }
+  }
+}
+
+provider "vercel" {
+  # Configuration options
+}
+
+terraform {
+  backend "s3" {
+    encrypt = true
+    bucket = "terraform-state-vercel-project-prod"
+    key = "terraform-state-vercel-project-prod/security/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+}
